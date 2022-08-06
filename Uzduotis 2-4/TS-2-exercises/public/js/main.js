cabbac08ac64f2db09b465deffa9146b49019550
Justas Sabaliauskas <sabaliauskas.justas@gmail.com>
2022-08-06 15:07:15 +0300

    file name changed

---
+++ Uzduotis 2-4/TS-2-exercises/public/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+console.groupCollapsed('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7'); {
+    const sorted = [
+        { name: 'peach', price: 23 },
+        { name: 'lemonade', price: 90 },
+        { name: 'lime', price: 432 },
+        { name: 'lemonades', price: 1 },
+    ];
+    sorted.sort((a, b) => a.price - b.price);
+    console.log(sorted);
+}
+console.groupEnd();
+console.groupCollapsed('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau'); {}
+console.groupEnd();
+//# sourceMappingURL=main.js.map
