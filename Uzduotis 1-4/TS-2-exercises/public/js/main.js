81eb00ebedcb5302f4909bcdc4168c5b141beb03
Justas Sabaliauskas <sabaliauskas.justas@gmail.com>
2022-08-06 13:45:14 +0300

    chore: first task

--- Uzduotis 1-4/TS-2-exercises/public/js/main.js
+++ Uzduotis 1-4/TS-2-exercises/public/js/main.js
@@ -1,11 +1,21 @@
 "use strict";
 console.groupCollapsed('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7');
 {
-    console.log('labas');
+    const sorted = [
+        { name: 'peach', price: 23 },
+        { name: 'lemonade', price: 90 },
+        { name: 'lime', price: 432 },
+        { name: 'lemonades', price: 1 },
+    ];
+    sorted.sort((a, b) => a.price - b.price);
+    console.log(sorted);
 }
 console.groupEnd();
 console.groupCollapsed('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau');
 {
+    Test.assertSimilar(addName({}, 'Brutus', 300), { Brutus: 300 });
+    Test.assertSimilar(addName({ piano: 500 }, 'Brutus', 400), { piano: 500, Brutus: 400 });
+    Test.assertSimilar(addName({ piano: 500, stereo: 300 }, 'Caligula', 440), { piano: 500, stereo: 300, Caligula: 440 });
 }
 console.groupEnd();
 //# sourceMappingURL=main.js.map
--- Uzduotis 1-4/TS-2-exercises/src/main.ts
+++ Uzduotis 1-4/TS-2-exercises/src/main.ts
@@ -1,11 +1,23 @@
+/* eslint-disable no-empty */
+/* eslint-disable no-lone-blocks */
 console.groupCollapsed('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7');
 {
-  console.log('labas');
+  const sorted:any[] = [
+    { name: 'peach', price: 23 },
+    { name: 'lemonade', price: 90 },
+    { name: 'lime', price: 432 },
+    { name: 'lemonades', price: 1 },
+  ];
+
+  sorted.sort((a, b) => a.price - b.price);
+  console.log(sorted);
 }
 console.groupEnd();
 
 console.groupCollapsed('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau');
 {
-
+  Test.assertSimilar(addName({}, 'Brutus', 300), { Brutus: 300 });
+  Test.assertSimilar(addName({ piano: 500 }, 'Brutus', 400), { piano: 500, Brutus: 400 });
+  Test.assertSimilar(addName({ piano: 500, stereo: 300 }, 'Caligula', 440), { piano: 500, stereo: 300, Caligula: 440 });
 }
 console.groupEnd();
