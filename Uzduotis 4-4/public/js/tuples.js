97f9d6072c42f64d458e6d28340b7ad3ffbc9c5f
Justas Sabaliauskas <sabaliauskas.justas@gmail.com>
2022-08-06 19:38:49 +0300

    tuples complete

--- Uzduotis 4-4/public/js/tuples.js
+++ Uzduotis 4-4/public/js/tuples.js
@@ -1,7 +1,7 @@
 "use strict";
-const person1 = ["Koridas", "Atmazas"];
-const person2 = ["Paklodijus", "Pagalvė"];
-const person3 = ["Višta", "Maumedienė"];
+const person1 = ['Koridas', 'Atmazas'];
+const person2 = ['Paklodijus', 'Pagalvė'];
+const person3 = ['Višta', 'Maumedienė'];
 let currentAge = 18;
 let currentHeight = 180;
 const age = [currentAge, () => currentAge += 1];
@@ -9,18 +9,38 @@ const height = [currentAge, (h) => {
         if (h)
             currentHeight = h;
     }];
+console.log(height);
 console.group('Tuples - užduotys');
 {
     console.group('1. Sukurkite žmogaus vardo ir amžiaus rinkinį');
     {
+        const personOne = ['Justas', 'Sabaliauskas'];
+        const personTwo = ['Greta', 'Rackauskaite'];
+        console.log(personOne, personTwo);
     }
     console.groupEnd();
     console.group('2. Sukurkite šuns šeimininko ir šuns rinkinį(pradžioje teks sukurti Šuns ir Šeiminiko tipus)');
     {
+        const dogOwnerDataObj = {
+            owner: 'OwnerThree',
+            dogBreed: 'BlackDog',
+            secondDogBreed: 'angel',
+            ownerBefore: {
+                ownerTwo: 'OwnerTwo',
+            },
+        };
+        console.log(dogOwnerDataObj);
+        const dogOwnerOne = ['Suns', 'oweneris'];
+        const dog = ['Grikis', 'Grikiaukskas'];
+        console.log(dog, dogOwnerOne);
     }
     console.groupEnd();
     console.group('3. Sukurkite trikampio taškų 2D erdvėje rinkinį(pradžioje teks sukurti 2D Taško tipą)');
     {
+        const triangle2D = [{ top: [1, 2] }, { left: [3, 4] }, { right: [5, 2] }];
+        const triangle2DTwo = [[1, 2], [3, 2], [4, 3]];
+        console.log(triangle2D);
+        console.log(triangle2DTwo);
     }
     console.groupEnd();
 }
--- Uzduotis 4-4/src/tuples.ts
+++ Uzduotis 4-4/src/tuples.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable no-lone-blocks */
 /*
   Tuple(rinkinys) - tai masyvas su greižtai nurodytais masyvo elemento tipais ir jų seka.
   Šis tipas neleidžia turėti kitokį kiekį, ar kitokio tipo elementų
@@ -5,41 +7,83 @@
 type FullnameTuple = [string, string];
 type UseNumericStateTuple = [number, (num?: number) => void];
 
-const person1: FullnameTuple = ["Koridas", "Atmazas"];
-const person2: FullnameTuple = ["Paklodijus", "Pagalvė"];
-const person3: FullnameTuple = ["Višta", "Maumedienė"];
+const person1: FullnameTuple = ['Koridas', 'Atmazas'];
+const person2: FullnameTuple = ['Paklodijus', 'Pagalvė'];
+const person3: FullnameTuple = ['Višta', 'Maumedienė'];
 
 let currentAge: number = 18;
 let currentHeight: number = 180;
-const age: UseNumericStateTuple = [currentAge, () => currentAge += 1];
+const age: UseNumericStateTuple = [currentAge, () => currentAge += 1];/// NELABAI SUPRATAU SITO
 const height: UseNumericStateTuple = [currentAge, (h) => {
   if (h) currentHeight = h;
 }];
+console.log(height);
 
 // Alikite užduotis, ir atspausdinkite konsolėje savo sprendimo pavyzdžius užduoties bloke
 console.group('Tuples - užduotys');
 {
   // ↓↓↓↓ Tipus apraškite čia ↓↓↓↓
+type FullNameTuplee = [string, string]
+type NameAndNumberTuplee = [number, (num?:number)=> void]
+type DogOwner = [string, string]
+type Bulldog='Bulldog'
+type BlackDog='BlackDog'
+type RedDog='RedDog'
+type OwenerOne='OwenerOne'
+type OwnerTwo='OwnerTwo'
+type OwnerThree='OwnerThree'
+type SecondOwnerDog= OwenerOne|OwnerTwo|OwnerThree
 
-  // ↑↑↑↑ Tipus apraškite čia ↑↑↑↑
+type Triangle= [{top:[number, number]}, {left:[number, number]}, {right:[number, number]}]
+type ShortScript =[number, number]
+type TriangleTwo= [ShortScript, ShortScript, ShortScript]
+// ↑↑↑↑ Tipus apraškite čia ↑↑↑↑
 
-  console.group('1. Sukurkite žmogaus vardo ir amžiaus rinkinį');
-  {
-    // sprendimo pavyzdžius spausdinkite čia 
-  }
-  console.groupEnd();
+console.group('1. Sukurkite žmogaus vardo ir amžiaus rinkinį');
+{
+  const personOne:FullNameTuplee = ['Justas', 'Sabaliauskas'];
+  const personTwo:FullNameTuplee = ['Greta', 'Rackauskaite'];
 
-  console.group('2. Sukurkite šuns šeimininko ir šuns rinkinį(pradžioje teks sukurti Šuns ir Šeiminiko tipus)');
-  {
-    // sprendimo pavyzdžius spausdinkite čia 
-  }
-  console.groupEnd();
+  console.log(personOne, personTwo);
+}
+console.groupEnd();
 
-  console.group('3. Sukurkite trikampio taškų 2D erdvėje rinkinį(pradžioje teks sukurti 2D Taško tipą)');
-  {
-    // sprendimo pavyzdžius spausdinkite čia 
-  }
-  console.groupEnd();
+console.group('2. Sukurkite šuns šeimininko ir šuns rinkinį(pradžioje teks sukurti Šuns ir Šeiminiko tipus)');
+{
+type DogWrapperObj={
+  owner:SecondOwnerDog,
+  dogBreed:BlackDog,
+  secondDogBreed?: string,
+  ownerBefore:{
+    ownerTwo:OwnerTwo
+  },
 
+}
+
+const dogOwnerDataObj:DogWrapperObj = {
+  owner: 'OwnerThree',
+  dogBreed: 'BlackDog',
+  secondDogBreed: 'angel',
+  ownerBefore: {
+    ownerTwo: 'OwnerTwo',
+  },
+};
+console.log(dogOwnerDataObj);
+
+const dogOwnerOne: DogOwner = ['Suns', 'oweneris'];
+const dog:Dog = ['Grikis', 'Grikiaukskas'];
+console.log(dog, dogOwnerOne);
+}
+
+console.groupEnd();
+
+console.group('3. Sukurkite trikampio taškų 2D erdvėje rinkinį(pradžioje teks sukurti 2D Taško tipą)');
+{
+  const triangle2D:Triangle = [{ top: [1, 2] }, { left: [3, 4] }, { right: [5, 2] }];
+  const triangle2DTwo:TriangleTwo = [[1, 2], [3, 2], [4, 3]];
+  console.log(triangle2D);
+  console.log(triangle2DTwo);
+}
+console.groupEnd();
 }
 console.groupEnd();
